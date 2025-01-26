bd6d47759ead1acd1563650006ca133e4d6060bf
Andrea Epifani <andy.epifani@gmail.com>
2025-01-26 10:44:20 +1000

    Logo link

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,20 +1,19 @@
 <template>
   <div :class="cn('flex items-center gap-4', isHome ? 'py-12' : 'p-4 border-b border-slate')">
-    <img src="/assets/logo.png" alt="Menelmacar" class="h-12 w-12">
-    <h1 class="text-xl font-semibold text-white">
-      Menelmacar
-    </h1>
+    <router-link to="/" class="flex items-center gap-2 text-light-grey opacity-80 hover:opacity-100">
+      <img src="/assets/logo.png" alt="Menelmacar" class="h-12 w-12">
+      <h1 class="text-xl font-semibold text-white">
+        Menelmacar
+      </h1>
+    </router-link>
 
     <div v-if="!isHome" class="flex items-center gap-4 pl-8">
-      <router-link to="/" class="text-light-grey opacity-80 hover:opacity-100">
-        Home
-      </router-link>
       <router-link to="/#faq" class="text-light-grey opacity-80 hover:opacity-100">
         FAQ
       </router-link>
     </div>
 
-    <ModelSelect v-if="!isHome" inline />
+    <ModelSelect v-if="!isHome" inline/>
   </div>
 </template>
 
